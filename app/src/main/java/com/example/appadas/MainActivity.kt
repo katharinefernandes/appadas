cd841184f24788da610efc7c5573ab016e1d8fb1
katharinefernandes <katharinefv@gmail.com>
2025-09-03 13:54:52 -0300

    feat: add delay to anwser and internacionalization

--- app/src/main/java/com/example/appadas/MainActivity.kt
+++ app/src/main/java/com/example/appadas/MainActivity.kt
@@ -1,23 +1,29 @@
 package com.example.appadas
 
+import android.content.Context
+import android.content.Intent
+import android.content.res.Configuration
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.appadas.databinding.ActivityMainBinding
+import java.util.Locale
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    private var numberJokerLast: Number = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater);
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         binding.btTellJoker.setOnClickListener {
             showJoker()
@@ -25,12 +31,37 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showJoker() {
-        val jokers = resources.getStringArray(R.array.jokers)
-        val numberJoker = Random.nextInt(jokers.size)
+        binding.tvQuestionJoker.visibility = View.VISIBLE
+        binding.tvAnswerJoker.visibility = View.GONE
+
+        val jokers = resources.getStringArray(R.array.jokers_questions)
+        var numberJoker = Random.nextInt(jokers.size)
+
+        while (numberJoker == numberJokerLast) {
+            numberJoker = Random.nextInt(jokers.size)
+        }
+
         val joker = jokers[numberJoker]
-        binding.tvJoker.text = joker
-        playSong()
+        numberJokerLast = numberJoker
+        binding.tvQuestionJoker.text = joker
+        showAnswerJoker(numberJoker)
     }
+
+    private fun showAnswerJoker(numberJoker: Int) {
+        val timeDelay: Long = 2000
+
+        val jokers = resources.getStringArray(R.array.jokers_answers)
+        val joker = jokers[numberJoker]
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            binding.tvQuestionJoker.visibility = View.GONE
+            binding.tvAnswerJoker.visibility = View.VISIBLE
+
+            binding.tvAnswerJoker.text = joker
+            playSong()
+        }, timeDelay);
+    }
+
     private fun playSong() {
         val mediaPlayer = MediaPlayer.create(this, R.raw.badumtss)
         mediaPlayer.start()
